90dd73a4e84e5fba23119d83ad68ecc075fbb21b
Subin Lee <chandar.sblee@gmail.com>
2025-03-06 11:35:37 +0900

    Update Config-dist.cs

--- Messaging_SDK_For_.NET_Core3.1_CSharp/Config-dist.cs
+++ Messaging_SDK_For_.NET_Core3.1_CSharp/Config-dist.cs
@@ -1,4 +1,5 @@
-﻿// 파일이름을 Config.cs로 변경, 클래스 이름을 Config로 변경 후 사용하세요
+﻿// 파일이름을 Config.cs로 변경, 클래스 이름을 Config로 변경 후 사용하세요. 
+// 반드시 API Key와 API Secret Key가 외부에 노출되지 않도록 주의해주세요!
 static class Config_Dist
 {
     public static string apiKey = "INPUT YOUR API KEY";
